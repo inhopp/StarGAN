81b734838460de8b4ca0d6cca9c8ba058db2a473
jeong.inho <inhonanananna@gmail.com>
2023-06-08 11:26:03 +0900

    add datasets

---
+++ data/__init__.py
@@ -0,0 +1,30 @@
+from .dataset import Dataset
+import torch
+import torchvision.transforms as transforms
+
+
+def generate_loader(phase, opt):
+    dataset = Dataset
+    crop_size = opt.crop_size
+    img_size = opt.input_size
+
+    mean = (0.5, 0.5, 0.5)
+    std = (0.5, 0.5, 0.5)
+
+    transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.CenterCrop(crop_size),
+        transforms.Resize(img_size),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std),
+    ])
+
+    dataset = dataset(opt, transform=transform)
+
+    kwargs = {
+        "batch_size": opt.batch_size,
+        "shuffle": True,
+        "drop_last": True,
+    }
+
+    return torch.utils.data.DataLoader(dataset, **kwargs)
---
+++ option.py
@@ -0,0 +1,50 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--seed", type=int, default=1)
+    parser.add_argument("--multigpu", type=bool, default=True)
+    parser.add_argument("--device", type=str, default="0")
+
+    # models
+    parser.add_argument("--pretrained", type=bool, default=False)
+    parser.add_argument("--crop_size", type=int, default=178)
+    parser.add_argument("--input_size", type=int, default=128)
+
+    # dataset
+    parser.add_argument("--data_dir", type=str, default="./datasets/")
+
+    # training setting
+    parser.add_argument("--lr", type=float, default=0.0002)
+    parser.add_argument("--cls_lambda", type=float, default=1)
+    parser.add_argument("--rec_lambda", type=float, default=10)
+    parser.add_argument("--gp_lambda", type=float, default=10)
+    parser.add_argument('--n_critic', type=int, default=5,
+                        help='number of D updates per each G update')
+    parser.add_argument("--b1", type=float, default=0.5,
+                        help="adam: decay of first order momentum of gradient")
+    parser.add_argument("--b2", type=float, default=0.999,
+                        help="adam: decay of first order momentum of gradient")
+    parser.add_argument("--n_epoch", type=int, default=100)
+    parser.add_argument("--num_workers", type=int, default=4)
+    parser.add_argument("--batch_size", type=int, default=16)
+
+    # misc
+    parser.add_argument("--ckpt_root", type=str, default="./FT_model")
+
+    return parser.parse_args()
+
+
+def make_template(opt):
+    # device
+    opt.device_ids = [int(item) for item in opt.device.split(',')]
+    if len(opt.device_ids) == 1:
+        opt.multigpu = False
+    opt.gpu = opt.device_ids[0]
+
+
+def get_option():
+    opt = parse_args()
+    make_template(opt)
+    return opt
